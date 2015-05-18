20b6f945c98d1fceec85d4e7151f1ddf9fe28947
zaoldyeck <zaoldyeck9970@gmail.com>
2015-05-19 02:09:51 +0800

    Edit to use gulp-load-plugins.

--- 03-livereload-refactored/gulpfile.js
+++ 03-livereload-refactored/gulpfile.js
@@ -1,37 +1,34 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect'),
-  watch = require('gulp-watch'),
-  less = require('gulp-less'),
-  coffee = require('gulp-coffee');
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
-gulp.task('webserver', function() {
-  connect.server({
-    livereload: true,
-    root: ['.', '.tmp']
-  });
+gulp.task('webserver', function () {
+    plugins.connect.server({
+        livereload: true,
+        root: ['.', '.tmp']
+    });
 });
 
-gulp.task('livereload', function() {
-  gulp.src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
-    .pipe(watch())
-    .pipe(connect.reload());
+gulp.task('livereload', function () {
+    gulp.src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
+        .pipe(plugins.watch())
+        .pipe(plugins.connect.reload());
 });
 
-gulp.task('less', function() {
-  gulp.src('styles/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('.tmp/styles'));
+gulp.task('less', function () {
+    gulp.src('styles/main.less')
+        .pipe(plugins.less())
+        .pipe(gulp.dest('.tmp/styles'));
 });
 
-gulp.task('coffee', function() {
-  gulp.src('scripts/*.coffee')
-    .pipe(coffee())
-    .pipe(gulp.dest('.tmp/scripts'));
+gulp.task('coffee', function () {
+    gulp.src('scripts/*.coffee')
+        .pipe(plugins.coffee())
+        .pipe(gulp.dest('.tmp/scripts'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('styles/*.less', ['less']);
-  gulp.watch('scripts/*.coffee', ['coffee']);
+gulp.task('watch', function () {
+    gulp.watch('styles/*.less', ['less']);
+    gulp.watch('scripts/*.coffee', ['coffee']);
 })
 
 gulp.task('default', ['less', 'coffee', 'webserver', 'livereload', 'watch']);
